c60ee9b3550c52f12fb9f820709c4ce6f74e2c00
baatar04 <baatar04@gmail.com>
2021-02-27 16:27:25 +0800

    login heseg duusgav

---
+++ js/login.js
@@ -0,0 +1,7 @@
+let $login = document.querySelector('.loginBtn');
+console.log('hello world');
+
+$login.addEventListener('click', () => {
+    console.log("hello world");
+    location.replace('profile-select.html');
+});
---
+++ js/profile-select.js
@@ -0,0 +1,23 @@
+// modal //
+
+let $modal = document.querySelector('.modal');
+let $button = document.querySelector('#modalshowme');
+
+$button.addEventListener('click', () => {
+    $modal.classList.add('showme');
+});
+
+let $modalExit = $modal.querySelector('.x');
+
+$modalExit.addEventListener('click', () => {
+    $modal.classList.remove('showme');
+});
+
+// logout //
+
+let $logout = document.querySelector('.logOut');
+
+
+$logout.addEventListener('click', () => {
+    location.replace('login.html');
+});
