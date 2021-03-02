99976cf05da242f843e24aeea423d887db4b8ddb
Battsetseg <bttstsg9090@gmail.com>
2021-03-02 14:37:48 +0800

    edit buttons onclicked

--- schedule.js
+++ schedule.js
@@ -1,5 +1,6 @@
+// Add todo button
 document.querySelector('.add-todo').onclick= function(){
-      console.log('hello world');
+      console.log('hello add todo button');
       var $modulTodo = document.querySelector('.modul-todo');
          if($modulTodo.style.display === 'none'){
             $modulTodo.style.display = 'block'
@@ -8,5 +9,15 @@ document.querySelector('.add-todo').onclick= function(){
          }     
 }
 
-
-
+//... edit delete button 
+var $kebabBtn = document.querySelectorAll('.kebab');
+for(var i = 0; i < $kebabBtn.length; i++){
+   console.log(i);
+   $kebabBtn[i].onclick = function(){
+         if(this.children[1].style.display === 'none'){
+            this.children[1].style.display = 'block';
+         } else {
+            this.children[1].style.display = 'none';
+         }
+      }
+   }
