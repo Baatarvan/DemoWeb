fcf703bec8df5e6f73762d3f6f2e5da477dc071e
Baatarvan <baatarvan@Dont-switch-name-iMac.local>
2021-03-04 12:57:07 +0800

    add children

--- js/child.controller.js
+++ js/child.controller.js
@@ -1,7 +1,8 @@
 let children = [];
 
+// draw children
+
 function drawChildrenFromSnapshot (snapshot) {
-  console.log("updated");
   children = [];
   snapshot.forEach((doc) => {
     children.push(doc.data());
@@ -9,60 +10,27 @@ function drawChildrenFromSnapshot (snapshot) {
     drawChildren(children);
 }
 
-let submit = document.querySelector('#btn');
-let $name = document.querySelector('#name');
-let $nas = document.querySelector('#nas');
-let box = document.querySelector('.wishlist');
+// add child
 
-// create element
-function addWish(doc) {
-    console.log(doc.id);
-    const $wish = `<div data-id='${doc.id}'>
-        <h1>${doc.data().name}</h1>
-        <p>${doc.data().nas}</p>
-        <button class="delete">delete1</button>
-    </div>`
+let $addChild = document.querySelector('.finishButton');
+let $name = document.querySelector('#childName');
+let $pin = document.querySelector('#childPin');
 
-    box.insertAdjacentHTML('beforeend', $wish);
-
-    let btnDelete = document.querySelector(`[data-id='${doc.id}'] .delete`);
-
-    btnDelete.onclick = function () {
-    db.collection('family').doc(`${doc.id}`).delete().then(() => {
-        console.log('Document succesfully deleted!');
-        }).catch(err => {
-        console.log('Error removing document', err);
-        });
-  }
+$addChild.onclick = () => {
+  db.collection("family").doc("DSfi2IoefMBltjwX55WC")
+  .collection('children').add({
+    name: $name.value,
+    pin: $pin.value,
+  })
+  .then(() => {
+      console.log("Document successfully written!");
+  })
+  .catch((error) => {
+      console.error("Error writing document: ", error);
+  });
 }
 
-// add wish
-
-// submit.addEventListener('click', () => {
-//     db.collection('family').add({
-//         name: $name.value,
-//         nas: $nas.value,
-//     });
-// })
-
 // Realtime
-
-// db.collection('family').onSnapshot(snapshot => {
-//     snapshot.docChanges().forEach(change => {
-//       if(change.type === 'added') {
-//         addWish(change.doc);
-//       }
-//       if(change.type === 'removed') {
-//         let item = document.querySelector(`[data-id='${change.doc.id}']`);
-//         box.removeChild(item);
-//       }
-//       if(change.type === 'modified') {
-//         let item = document.querySelector(`[data-id='${change.doc.id}']`);
-//         box.removeChild(item);
-//         addWish(change.doc);
-//       }
-//     })
-// })
 
 window.onload = function() {
   db.collection('family')
@@ -70,5 +38,3 @@ window.onload = function() {
     .collection('children')
     .onSnapshot(drawChildrenFromSnapshot);
 };
-
-
--- js/child.view.js
+++ js/child.view.js
@@ -1,17 +1,14 @@
-
+// create element
 function $createChild(data) {
     var $child = document.createElement('div');
+    $child.classList.add('child flex');
     var content = `
-        <div class="child flex">
-            <img class="avatarimg" src="https://api.getepic.com/utils/compose.png?avatar_id=15&frame_id=1&size=2x&style_type=avatar" alt="avatar" width="170px">
-            <h3 class="name">${data.name}</h3>
-        </div>
+        <img class="avatarimg" src="https://api.getepic.com/utils/compose.png?avatar_id=15&frame_id=1&size=2x&style_type=avatar" alt="avatar" width="170px">
+        <h3 class="name">${data.name}</h3>
     `;
-    $child.innerHTML = content
-
+    $child.innerHTML = content;
     return $child;
 }
-
 
 // Main draw function
 function drawChildren(children) {
@@ -22,5 +19,4 @@ function drawChildren(children) {
         $child = $createChild(item);
         $childrenList.append($child);
     });
-
 }
